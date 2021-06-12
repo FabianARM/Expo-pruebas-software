20b46749c9a1e0bc5d0fcd0b9da2a189398b8f97
Fabian <fabian.rojasmasis@ucr.ac.cr>
2021-06-12 15:50:04 -0600

    primer commit

---
+++ src/fechas/Fechas.java
@@ -0,0 +1,140 @@
+package fechas;
+//
+import java.io.IOException;
+
+class Fechas {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        // 
+
+    	System.out.println("Programa principal");
+    	
+    	System.out.println(diaDelAno(2017, 12,31));  // Debe imprimir 365
+    	
+    	System.out.println(esFechaValida(2003,2,28)); //Debe imprimir true
+    	
+        }
+    //}
+ 
+    public static boolean esFechaValida(final int year, final int month, final int dayOfMonth){
+      
+    int[] DAYS_IN_MONTH = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    int FEBRUARY = 2;
+      
+    if((dayOfMonth >= 1 && dayOfMonth <= 31) && (month >= 1 && month <= 12) && (year >= 1582)){
+      
+      if((esAnoBisiesto(year) && (month == FEBRUARY) && (dayOfMonth == 29))  
+        || (dayOfMonth <= DAYS_IN_MONTH[month])){
+        
+        return true; 
+      
+      }else{
+        return false;
+      }
+      
+    }else{
+      return false;
+    }
+    }
+    
+    public static int diaDelAno(int year, int month, int dayOfMonth) {
+        int totalDays = validarDiasDeMes(month, dayOfMonth,year);
+        if (totalDays != -1 && totalDays != -2) {
+            totalDays = validarAno(year);
+            if (totalDays != -3) {                
+                int daysPerMonthArray[]
+                        = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+                totalDays = dayOfMonth;
+                for (int i = 1; i < month; i++) {
+                    totalDays += daysPerMonthArray[i-1];
+                }
+                
+                if(esAnoBisiesto(year) && month > 2) {
+                  totalDays++;
+                }
+            }
+        }
+        return totalDays;
+    }
+       
+   public static int validarDiasDeMes(int month, int dayOfMonth, int year) {
+        int isNotCorrect = validarMes(month);
+        if (isNotCorrect > -1) {
+            switch (month) {
+                case 2://febraury days in month: 28
+                    if (dayOfMonth < 1 || dayOfMonth > 28) {
+                        if(!esAnoBisiesto(year)){
+                          isNotCorrect = -2;
+                        }
+                    }
+                    if (dayOfMonth < 1 || dayOfMonth > 29) {
+                        if(esAnoBisiesto(year)){
+                          isNotCorrect = -2;
+                        }
+                    }
+                    break;
+                case 1:   
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 11:
+                case 12://months with 31 days
+                    if (dayOfMonth < 1 || dayOfMonth > 31) {
+                        isNotCorrect = -2;
+                    }
+                    break;
+                default://month with 30 days
+                    if (dayOfMonth < 1 || dayOfMonth > 30) {
+                          isNotCorrect = -2;
+                    }
+                    break;
+            }
+        }
+        return isNotCorrect; 
+    }
+
+     
+    public static int[] devuelveArray (int dia, int mes, int ano) {
+          
+    int[] arregloParaDevolver ; //= {0,0,0};
+    arregloParaDevolver = new int[3];
+          
+    arregloParaDevolver[0] = dia+1;
+    arregloParaDevolver[1] = mes+1;
+    arregloParaDevolver[2] = ano+1; 
+    return arregloParaDevolver;
+    }
+        
+    public static int validarMes(int pmonth) {
+        int month = pmonth;
+        if (month > 12 || month < 1) {   
+            month = -1;//month not correct
+        }
+        return month;
+    }
+    
+    public static int validarAno(int pyear) {
+        int yeay = pyear;
+        if (yeay < 1582) {
+            yeay = -3;//year not correct
+        }
+        return yeay;
+    }
+    
+    public static boolean esAnoBisiesto(int pyear){
+        boolean isLeap = false;
+        if (validarAno(pyear) > -3) {        
+              if ((pyear % 400 == 0) | (pyear % 4 == 0 & pyear % 100 != 0)) {
+                  isLeap = true;
+              } 
+             }  
+        return isLeap;
+    }
+}
+
---
+++ src/fechas/FechasUnitTests.java
@@ -0,0 +1,124 @@
+package fechas;
+//
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FechasUnitTests {
+
+    @Test
+    public void pruebaDiaDelAno() {    
+      assertEquals (Fechas.diaDelAno(2017,12,31),365);  
+      assertEquals (Fechas.diaDelAno(2017,1,2),2);
+      assertEquals (Fechas.diaDelAno(2017,5,2),122);
+      assertEquals (Fechas.diaDelAno(2020,12,31),366);
+      assertEquals(Fechas.diaDelAno(2020, 1, 1), 1);
+    }
+ 
+    @Test
+    public void pruebaFechaValida() {
+      assertEquals(Fechas.esFechaValida(2017,7,35),false);
+      assertEquals(Fechas.esFechaValida(2003,2,28),true);
+      assertEquals(Fechas.esFechaValida(2003,2,0),false);
+      assertEquals(Fechas.esFechaValida(2003,0,28),false);
+      assertEquals(Fechas.esFechaValida(2020,9,1),true);
+      assertEquals(Fechas.esFechaValida(2020,1,23),true);
+      assertEquals(Fechas.esFechaValida(1582,9,23),true);
+      assertEquals(Fechas.esFechaValida(2020,5,31),true);
+      assertEquals(Fechas.esFechaValida(2020,3,30),true);
+      assertEquals(Fechas.esFechaValida(2020,2,29),true);
+      assertEquals(Fechas.esFechaValida(2020,2,28),true);
+    }
+    
+    @Test 
+    public void pruebaEsAnnoBisiestoValido() {
+        int ANNO_BISIESTO_1 = 1600; 
+        assertEquals(Fechas.esAnoBisiesto(ANNO_BISIESTO_1), true);
+    }
+
+    @Test
+    public void pruebaFechaInvalida() {
+        // ANO, MES y DIA MES
+        assertEquals(Fechas.esFechaValida(2020, 9, 32), false);
+        assertEquals(Fechas.esFechaValida(2020, 13, 23), false);
+        assertEquals(Fechas.esFechaValida(1581, 9, 23), false);
+        assertEquals(Fechas.esFechaValida(2020, 5, 32), false);
+        assertEquals(Fechas.esFechaValida(2020, 4, 31), false);
+        assertEquals(Fechas.esFechaValida(2020, 2, 30), false);
+        assertEquals(Fechas.esFechaValida(2019, 2, 29), false);
+
+    }
+    
+    @Test
+    public void pruebaValidarDiasDeMesInvalido() {
+    	assertEquals(Fechas.validarDiasDeMes(2, 0, 2020), -2);
+    	assertEquals(Fechas.validarDiasDeMes(2, 30, 2020), -2);
+    	assertEquals(Fechas.validarDiasDeMes(2, 30, 2019), -2);
+    	assertEquals(Fechas.validarDiasDeMes(2, 29, 2019), -2);
+    	assertEquals(Fechas.validarDiasDeMes(3, 0, 2020), -2);
+    	assertEquals(Fechas.validarDiasDeMes(3, 32, 2020), -2);
+    	assertEquals(Fechas.validarDiasDeMes(4, 0, 2020), -2);
+    	assertEquals(Fechas.validarDiasDeMes(4, 31, 2020), -2);
+    	assertEquals(Fechas.validarDiasDeMes(-1, 31, 2020), -1);
+    }
+    
+    @Test
+    public void pruebaValidarDiasDeMesValido() {
+    	assertEquals(Fechas.validarDiasDeMes(2, 17, 2020), 2);
+    	assertEquals(Fechas.validarDiasDeMes(4, 17, 2020), 4);
+    }
+    
+    @Test
+    public void  pruebaValidarMesValido() {
+        int MES_VALIDO = 5; 
+        assertEquals(Fechas.validarMes(MES_VALIDO), MES_VALIDO);
+    }
+
+    @Test 
+    public void pruebaValidarMesInvalido() {
+        int MES_INVALIDO = 13;
+         assertNotEquals(Fechas.validarMes(MES_INVALIDO), MES_INVALIDO);
+         int MES_INVALIDO_2 = 0; 
+         assertNotEquals(Fechas.validarMes(MES_INVALIDO_2), MES_INVALIDO_2);
+    }
+
+    @Test 
+    public void pruebaValidarAnnoValido() {
+        int ANNO_VALIDO = 2020; 
+        assertEquals(Fechas.validarAno(ANNO_VALIDO), ANNO_VALIDO);
+    }
+    @Test 
+    public void pruebaValidarAnnoInvalido() {
+        int ANNO_INVALIDO = 15; 
+        assertNotEquals(Fechas.validarAno(ANNO_INVALIDO), ANNO_INVALIDO);
+    }
+
+    @Test 
+    public void pruebaValidarDevuelveArray() {
+        int DIA = 10; 
+        int MONTH = 01; 
+        int YEAR = 1998; 
+        int[] ARREGLO_FECHA = {DIA+1, MONTH+1, YEAR+1}; 
+        assertArrayEquals(Fechas.devuelveArray(DIA, MONTH, YEAR), ARREGLO_FECHA);
+    }
+  
+    @Test 
+    public void pruebaEsAnnoBisiestoInvalido() {
+        int ANO_INVALIDO = 1500; 
+        assertEquals(Fechas.esAnoBisiesto(ANO_INVALIDO), false);
+    }
+    
+    @Test
+    public void pruebaDiaDelAnoInvalido() {
+
+        assertEquals(Fechas.diaDelAno(2020, 30, 2), -1);
+
+        assertEquals(Fechas.diaDelAno(2020, 2, 30), -2);
+
+        assertEquals(Fechas.diaDelAno(1581, 3, 30), -3);
+
+
+    }
+    
+ }
+  
